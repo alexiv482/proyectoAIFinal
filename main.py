cad1a1f69acf4b9a1ec6b568faa8bef624967c40
Alex482 <erik.alexandra.estupinan@gmail.com>
2026-07-12 21:14:00 -0500

    Cimientos

---
+++ main.py
@@ -0,0 +1,4 @@
+from langchain_groq import ChatGroq
+from keys import GROQ_API_KEY
+from models import GROQ
+
---
+++ models.py
@@ -0,0 +1,3 @@
+GEMINI_FLASH = "gemini-1.5-flash"
+OPENAI = "openai/gpt-oss-120b"
+GROQ = "llama-3.3-70b-versatile"
